6f06d956e60e8b9c25b5fa38c88805b5cf556df7
satyamkumarjha1 <satyamkumarjha243@gmail.com>
2024-04-22 17:42:14 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_obx/movie.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  var count = 0.obs;
+  //var count = 0.obs;
   // final count = Rx<int>(0);
   // final count = RxInt(0);
+
+  var movie = Movie(name: 'Wanted', ticket: 100).obs;
 
   // This widget is the root of your application.
   @override
@@ -23,12 +26,14 @@ class MyApp extends StatelessWidget {
           children: [
             Obx(
               () => Text(
-                "$count",
+                "${movie.value.name} ",
               ),
             ),
             ElevatedButton(
               onPressed: () {
-                count++;
+                movie.value.name = 'satyam';
+                movie.refresh();
+                print(movie.value.name);
               },
               child: const Text("Increment"),
             ),
---
+++ lib/movie.dart
@@ -0,0 +1,12 @@
+
+// class Movie {
+//   var name = "Wanted".obs;
+//   var ticket = 200.obs;
+// }
+
+class Movie {
+  String? name;
+  int? ticket;
+
+  Movie({required this.name, required this.ticket});
+}
